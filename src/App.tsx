fcc70374c3f64c0c759b7ce92c37ac2401341795
msouza28 <matheus.emiliano28@gmail.com>
2024-05-03 16:00:33 -0300

    First Commit done

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { dummyData } from "./data/todos";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+
+export default function App() {
+
+const [todos, setTodos] = useState(dummyData)
+
+function setTodoCompleted(id:number, completed:boolean){
+  setTodos((prevTodos) => 
+    prevTodos.map((todo) => (todo.id === id ? {...todo, completed} : todo))
+);
+}
+
+function addTodo(title: string) {
+  setTodos(prevTodos => [
+    {
+      id: prevTodos.length + 1,
+      title,
+      completed: false
+    },
+    ...prevTodos,
+  ])
+}
+
+function deleteTodo(id:number){
+  setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+}
+  return (
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center uppercase">Tarefas diárias </h1>
+      <div className="max-w-lg mx-auto bg-slate-200 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList 
+        todos={todos}
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}
+        />
+      </div>
+    </main>
+  )
+}
---
+++ src/data/todos.ts
@@ -0,0 +1,9 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Marque como cluído para o seu dia ser maravilhoso",
+        completed:false
+    },
+]
